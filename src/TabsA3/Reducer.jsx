afa0c1c432863539b17a4b66f42a16267eb52673
Sourav-nath <35804676+Programmer-Sourav@users.noreply.github.com>
2023-08-11 10:11:43 +0530

    feat: practice a3 reducer initial commit
    mcr

--- src/TabsA3/Reducer.jsx
+++ src/TabsA3/Reducer.jsx
@@ -1,4 +1,18 @@
 export const initialState = {
-    data: [],
-    
+    data: [] ,
+    currentTab: ""
+}
+
+export const ACTION_TYPES = {
+    INITIALIZE: "INITIALIZE",
+    CURRENT_TAB: "CURRENT_TAB"
+}
+
+export default function Reducer (state, action){
+  switch(action.type){
+    case ACTION_TYPES.INITIALIZE:
+        return {...state}
+    case ACTION_TYPES.CURRENT_TAB: 
+         return {...state}     
+  }
 }
---
+++ src/TabsA3/TabComponent.jsx
@@ -0,0 +1,9 @@
+export default function TabComponent(){
+
+
+    return(
+        <div> 
+
+        </div>
+    )
+}
